670363ed0d632cae371772a45bf715852695e407
The Spine <dtorkelson@gmail.com>
2021-03-28 07:08:39 -0400

    add apt upgrade

--- client_setup.sh
+++ client_setup.sh
@@ -18,14 +18,14 @@ passwd ansible
 # At this point, the orchestrator needs to copy its ansible ssh key to this
 # client
 # Show this computer's IP address and wait: 
-# regex pattern is the first group of digits and periods
+# 	regex pattern is the first group of digits and periods
 ip_addr=$(hostname -I | sed -r 's/^([[:digit:]\.]+)\s.*$/\1/')
 echo "This client's ip: $ip_addr"
 read -p 'Press enter once SSH key has been copied to client...'
 
 # TODO: ensure PasswordAuthentication is set to yes in sshd_config
 # Can this be done without entering a text editor? What if I just
-# echoed the line to be appended to the config file? 
+#	echoed the line to be appended to the config file? 
 
 # Configure sudo so ansible user can execute commands
 echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible
